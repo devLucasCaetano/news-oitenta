0b218287705ef794af4c86c0344e6edbc1c99ff1
devLucasCaetano <lucas_caetano123@hotmail.com>
2022-11-20 10:57:07 -0300

    tabs bar template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { TabsBarComponent } from './components/tabs-bar/tabs-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent,
+    TabsBarComponent,
     FooterComponent
   ],
   imports: [CommonModule],
   exports: [
-    NavBarComponent,
+    TabsBarComponent,
     FooterComponent
   ]
 
